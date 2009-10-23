332280a3ca8715c38c0a4fedc8aa9593d2e3c795
abadi.rit <devnull@localhost>
2009-10-23 12:41:58 +0000

    Added line numbering

--- jbrick/src/ui/MainWindow.java
+++ jbrick/src/ui/MainWindow.java
@@ -1,5 +1,4 @@
-package  ui;
-
+package ui;
 
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
@@ -8,8 +7,9 @@ import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.DefaultUndoManager;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.IUndoManager;
+import org.eclipse.jface.text.source.CompositeRuler;
+import org.eclipse.jface.text.source.LineNumberRulerColumn;
 import org.eclipse.jface.text.source.SourceViewer;
-import org.eclipse.jface.text.source.VerticalRuler;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.window.ApplicationWindow;
@@ -47,80 +47,86 @@ import actions.UndoAction;
  * This class provides the main window of JBrickEditor
  */
 public class MainWindow extends ApplicationWindow implements
-    IPropertyChangeListener {
-  // The viewer
-  private SourceViewer viewer;
+		IPropertyChangeListener {
+	// The viewer
+	private SourceViewer viewer;
 
-  // The actions
-  private AboutAction aboutAction = new AboutAction();
-  private CopyAction copyAction = new CopyAction();
-  private CutAction cutAction = new CutAction();
-  private ExitAction exitAction = new ExitAction();
-  private FindAction findAction = new FindAction();
-  private NewAction newAction = new NewAction();
-  private OpenAction openAction = new OpenAction();
-  private PasteAction pasteAction = new PasteAction();
-  private PreferencesAction prefsAction = new PreferencesAction();
-  private PrintAction printAction = new PrintAction();
-  private RedoAction redoAction = new RedoAction();
-  private SaveAction saveAction = new SaveAction();
-  private SaveAsAction saveAsAction = new SaveAsAction();
-  private UndoAction undoAction = new UndoAction();
-  
-  private CompileAction compileAction = new CompileAction(); 
+	// The actions
+	private AboutAction aboutAction = new AboutAction();
+	private CopyAction copyAction = new CopyAction();
+	private CutAction cutAction = new CutAction();
+	private ExitAction exitAction = new ExitAction();
+	private FindAction findAction = new FindAction();
+	private NewAction newAction = new NewAction();
+	private OpenAction openAction = new OpenAction();
+	private PasteAction pasteAction = new PasteAction();
+	private PreferencesAction prefsAction = new PreferencesAction();
+	private PrintAction printAction = new PrintAction();
+	private RedoAction redoAction = new RedoAction();
+	private SaveAction saveAction = new SaveAction();
+	private SaveAsAction saveAsAction = new SaveAsAction();
+	private UndoAction undoAction = new UndoAction();
 
-  // The font
-  private Font font;
+	private CompileAction compileAction = new CompileAction();
 
-  // The undo manager
-  private IUndoManager undoManager;
+	// The font
+	private Font font;
+
+
+	// The undo manager
+	private IUndoManager undoManager;
 
   // Right Click Menu
   private MenuManager menuManager;
 
-  /**
-   * MainWindow constructor
-   */
-  public MainWindow() {
-    super(null);
-    addMenuBar();
-    addToolBar(SWT.FLAT);
-  }
 
-  /**
-   * Runs the application
-   */
-  public void run() {
-    setBlockOnOpen(true);
-    open();
-    Display.getCurrent().dispose();
-  }
+	/**
+	 * MainWindow constructor
+	 */
+	public MainWindow() {
+		super(null);
+		addMenuBar();
+		addToolBar(SWT.FLAT);
+	}
 
-  /**
-   * Configures the shell
-   * 
-   * @param shell the shell
-   */
-  protected void configureShell(Shell shell) {
-    super.configureShell(shell);
-    shell.setText("JBrick Editor");
-  }
+	/**
+	 * Runs the application
+	 */
+	public void run() {
+		setBlockOnOpen(true);
+		open();
+		Display.getCurrent().dispose();
+	}
 
-  /**
-   * Creates the main window's contents
-   * 
-   * @param parent the main window
-   * @return Control
-   */
-  protected Control createContents(Composite parent) {
-    // Create the viewer
-    viewer = new SourceViewer(parent, new VerticalRuler(10), SWT.V_SCROLL
-        | SWT.H_SCROLL);
+	/**
+	 * Configures the shell
+	 * 
+	 * @param shell
+	 *            the shell
+	 */
+	protected void configureShell(Shell shell) {
+		super.configureShell(shell);
+		shell.setText("JBrick Editor");
+	}
 
-    // Configure it and set the document
-    viewer.configure(new JBrickEditorSourceViewerConfiguration());
-    viewer.setDocument(JBrickEditor.getApp().getDocument());
+	/**
+	 * Creates the main window's contents
+	 * 
+	 * @param parent
+	 *            the main window
+	 * @return Control
+	 */
+	protected Control createContents(Composite parent) {
+		// Create the viewer
+		/*
+		 * viewer = new SourceViewer(parent, new VerticalRuler(10), SWT.V_SCROLL
+		 * | SWT.H_SCROLL);
+		 */
     
+
+
+		// //////////////
+		int VERTICAL_RULER_WIDTH = 12;
 
     // Menu manager initialize
     menuManager = new MenuManager();
@@ -135,173 +141,201 @@ public class MainWindow extends ApplicationWindow implements
     // Set any preferences
     loadPreferences();
 
-    // Create the undo manager
-    undoManager = new DefaultUndoManager(100);
-    undoManager.connect(viewer);
 
-    viewer.getTextWidget().setFocus();
+		int styles = SWT.V_SCROLL | SWT.H_SCROLL | SWT.MULTI | SWT.BORDER
+				| SWT.FULL_SELECTION;
+		
+		
+		CompositeRuler ruler = new CompositeRuler(VERTICAL_RULER_WIDTH);
+		
+        LineNumberRulerColumn lnrc = new LineNumberRulerColumn();
+        ruler.addDecorator(0,lnrc);
+        
+        
 
-    // Return the StyledText
-    return viewer.getTextWidget();
-  }
+		viewer = new SourceViewer(parent, ruler,	styles);
+		
+		
+		// ///////////
 
-  protected void loadPreferences() {
-    IPreferenceStore ps = JBrickEditor.getApp().getPreferences();
-    setWrap(ps.getBoolean(FileExtensionConstants.WRAP));
+		// Configure it and set the document
+		viewer.configure(new JBrickEditorSourceViewerConfiguration());
+		viewer.setDocument(JBrickEditor.getApp().getDocument());
 
-    String fontProp = ps.getString(FileExtensionConstants.FONT);
-    if (fontProp.length() > 0) {
-      FontData[] fd = new FontData[1];
-      fd[0] = new FontData(fontProp);
-      setFont(fd);
-    }
-  }
+		// Set any preferences
+		loadPreferences();
 
-  /**
-   * Updates the view with the preferences
-   *  
-   */
-  public void propertyChange(PropertyChangeEvent event) {
-    if (FileExtensionConstants.WRAP.equals(event.getProperty()))
-      setWrap(((Boolean) event.getNewValue()).booleanValue());
-    else if (FileExtensionConstants.FONT.equals(event.getProperty()))
-        setFont((FontData[]) event.getNewValue());
-  }
+		// Create the undo manager
+		undoManager = new DefaultUndoManager(100);
+		undoManager.connect(viewer);
 
-  /**
-   * Creates the menu manager
-   * 
-   * @return MenuManager
-   */
-  protected MenuManager createMenuManager() {
-    MenuManager mm = new MenuManager();
-    MenuManager fileMenu = new MenuManager("&File");
-    MenuManager editMenu = new MenuManager("&Edit");
-    MenuManager compileMenu = new MenuManager("&Compile");
-    MenuManager helpMenu = new MenuManager("&Help");
+		viewer.getTextWidget().setFocus();
 
-    mm.add(fileMenu);
-    mm.add(editMenu);
-    mm.add(compileMenu);
-    mm.add(helpMenu);
+		// Return the StyledText
+		return viewer.getTextWidget();
+	}
 
-    fileMenu.add(newAction);
-    fileMenu.add(openAction);
-    fileMenu.add(saveAction);
-    fileMenu.add(saveAsAction);
-    fileMenu.add(new Separator());
-    fileMenu.add(printAction);
-    fileMenu.add(new Separator());
-    fileMenu.add(exitAction);
+	protected void loadPreferences() {
+		IPreferenceStore ps = JBrickEditor.getApp().getPreferences();
+		setWrap(ps.getBoolean(FileExtensionConstants.WRAP));
 
-    editMenu.add(undoAction);
-    editMenu.add(redoAction);
-    editMenu.add(new Separator());
-    editMenu.add(cutAction);
-    editMenu.add(copyAction);
-    editMenu.add(pasteAction);
-    editMenu.add(new Separator());
-    editMenu.add(findAction);
-    editMenu.add(new Separator());
-    editMenu.add(prefsAction);
+		String fontProp = ps.getString(FileExtensionConstants.FONT);
+		if (fontProp.length() > 0) {
+			FontData[] fd = new FontData[1];
+			fd[0] = new FontData(fontProp);
+			setFont(fd);
+		}
+	}
 
-    compileMenu.add(compileAction);
-    
-    helpMenu.add(aboutAction);
+	/**
+	 * Updates the view with the preferences
+	 * 
+	 */
+	public void propertyChange(PropertyChangeEvent event) {
+		if (FileExtensionConstants.WRAP.equals(event.getProperty()))
+			setWrap(((Boolean) event.getNewValue()).booleanValue());
+		else if (FileExtensionConstants.FONT.equals(event.getProperty()))
+			setFont((FontData[]) event.getNewValue());
+	}
 
-    return mm;
-  }
+	/**
+	 * Creates the menu manager
+	 * 
+	 * @return MenuManager
+	 */
+	protected MenuManager createMenuManager() {
+		MenuManager mm = new MenuManager();
+		MenuManager fileMenu = new MenuManager("&File");
+		MenuManager editMenu = new MenuManager("&Edit");
+		MenuManager compileMenu = new MenuManager("&Compile");
+		MenuManager helpMenu = new MenuManager("&Help");
 
-  /**
-   * Creates the toolbar
-   * 
-   * @param style the style for the toolbar
-   * @return ToolBarManager
-   */
-  protected ToolBarManager createToolBarManager(int style) {
-    ToolBarManager tm = new ToolBarManager(style);
+		mm.add(fileMenu);
+		mm.add(editMenu);
+		mm.add(compileMenu);
+		mm.add(helpMenu);
 
-    // Add all the actions
-    tm.add(newAction);
-    tm.add(openAction);
-    tm.add(saveAction);
-    tm.add(saveAsAction);
-    tm.add(new Separator());
-    tm.add(printAction);
-    tm.add(new Separator());
-    tm.add(undoAction);
-    tm.add(redoAction);
-    tm.add(new Separator());
-    tm.add(cutAction);
-    tm.add(copyAction);
-    tm.add(pasteAction);
-    tm.add(new Separator());
-    tm.add(findAction);
-    tm.add(new Separator());
-    tm.add(prefsAction);
-    tm.add(new Separator());
-    tm.add(compileAction);
-    tm.add(new Separator());
-    tm.add(aboutAction);
+		fileMenu.add(newAction);
+		fileMenu.add(openAction);
+		fileMenu.add(saveAction);
+		fileMenu.add(saveAsAction);
+		fileMenu.add(new Separator());
+		fileMenu.add(printAction);
+		fileMenu.add(new Separator());
+		fileMenu.add(exitAction);
 
-    return tm;
-  }
+		editMenu.add(undoAction);
+		editMenu.add(redoAction);
+		editMenu.add(new Separator());
+		editMenu.add(cutAction);
+		editMenu.add(copyAction);
+		editMenu.add(pasteAction);
+		editMenu.add(new Separator());
+		editMenu.add(findAction);
+		editMenu.add(new Separator());
+		editMenu.add(prefsAction);
 
-  /**
-   * Gets the text viewer
-   * 
-   * @return ITextViewer
-   */
-  public ITextViewer getViewer() {
-    return viewer;
-  }
+		compileMenu.add(compileAction);
 
-  /**
-   * Sets the font
-   * 
-   * @param fontData
-   */
-  public void setFont(FontData[] fontData) {
-    // Create the font
-    Font temp = new Font(getShell().getDisplay(), fontData);
+		helpMenu.add(aboutAction);
 
-    // If creation succeeded, dispose the old font
-    if (font != null) font.dispose();
+		return mm;
+	}
 
-    // Use the new font
-    font = temp;
-    viewer.getTextWidget().setFont(font);
-  }
+	/**
+	 * Creates the toolbar
+	 * 
+	 * @param style
+	 *            the style for the toolbar
+	 * @return ToolBarManager
+	 */
+	protected ToolBarManager createToolBarManager(int style) {
+		ToolBarManager tm = new ToolBarManager(style);
 
-  /**
-   * Turns on/off word wrap
-   * 
-   * @param wrap true to wrap
-   */
-  public void setWrap(boolean wrap) {
-    viewer.getTextWidget().setWordWrap(wrap);
-  }
+		// Add all the actions
+		tm.add(newAction);
+		tm.add(openAction);
+		tm.add(saveAction);
+		tm.add(saveAsAction);
+		tm.add(new Separator());
+		tm.add(printAction);
+		tm.add(new Separator());
+		tm.add(undoAction);
+		tm.add(redoAction);
+		tm.add(new Separator());
+		tm.add(cutAction);
+		tm.add(copyAction);
+		tm.add(pasteAction);
+		tm.add(new Separator());
+		tm.add(findAction);
+		tm.add(new Separator());
+		tm.add(prefsAction);
+		tm.add(new Separator());
+		tm.add(compileAction);
+		tm.add(new Separator());
+		tm.add(aboutAction);
 
-  /**
-   * Gets the undo manager
-   * 
-   * @return IUndoManager
-   */
-  public IUndoManager getUndoManager() {
-    return undoManager;
-  }
+		return tm;
+	}
 
-  /**
-   * Closes the main window
-   */
-  public boolean close() {
-    boolean close = false;
-    if (JBrickEditor.getApp().checkOverwrite()) {
-      close = super.close();
-      if (close) {
-        if (font != null) font.dispose();
-      }
-    }
-    return close;
-  }
+	/**
+	 * Gets the text viewer
+	 * 
+	 * @return ITextViewer
+	 */
+	public ITextViewer getViewer() {
+		return viewer;
+	}
+
+	/**
+	 * Sets the font
+	 * 
+	 * @param fontData
+	 */
+	public void setFont(FontData[] fontData) {
+		// Create the font
+		Font temp = new Font(getShell().getDisplay(), fontData);
+
+		// If creation succeeded, dispose the old font
+		if (font != null)
+			font.dispose();
+
+		// Use the new font
+		font = temp;
+		viewer.getTextWidget().setFont(font);
+	}
+
+	/**
+	 * Turns on/off word wrap
+	 * 
+	 * @param wrap
+	 *            true to wrap
+	 */
+	public void setWrap(boolean wrap) {
+		viewer.getTextWidget().setWordWrap(wrap);
+	}
+
+	/**
+	 * Gets the undo manager
+	 * 
+	 * @return IUndoManager
+	 */
+	public IUndoManager getUndoManager() {
+		return undoManager;
+	}
+
+	/**
+	 * Closes the main window
+	 */
+	public boolean close() {
+		boolean close = false;
+		if (JBrickEditor.getApp().checkOverwrite()) {
+			close = super.close();
+			if (close) {
+				if (font != null)
+					font.dispose();
+			}
+		}
+		return close;
+	}
 }
