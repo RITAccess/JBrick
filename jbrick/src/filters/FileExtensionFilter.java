4c3797ec7d677fcd24dd610e1dd70978540ecefd
abadi.rit <devnull@localhost>
2009-10-30 12:34:31 +0000

    add filter

---
+++ jbrick/src/filters/FileExtensionFilter.java
@@ -0,0 +1,14 @@
+package filters;
+
+import java.io.File;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+public class FileExtensionFilter extends ViewerFilter {
+
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		return ((File) element).getName().endsWith(".java");
+	}
+
+}
