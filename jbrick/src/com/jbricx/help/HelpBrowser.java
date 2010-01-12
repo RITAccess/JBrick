0f607555ee7435e6ddcc097a05be1a9bbd82ff3d
sankaran.priya05 <devnull@localhost>
2010-01-12 21:18:31 +0000

    Included the button for Printing

--- jbrick/src/com/jbricx/help/HelpBrowser.java
+++ jbrick/src/com/jbricx/help/HelpBrowser.java
@@ -1,6 +1,9 @@
 package com.jbricx.help;
 
-
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.net.MalformedURLException;
@@ -21,9 +24,16 @@ import org.eclipse.swt.browser.TitleEvent;
 import org.eclipse.swt.browser.TitleListener;
 import org.eclipse.swt.browser.WindowEvent;
 import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.printing.PrintDialog;
+import org.eclipse.swt.printing.Printer;
+import org.eclipse.swt.printing.PrinterData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
@@ -37,232 +47,220 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 
-public class HelpBrowser{
- 
-	private Display display = Display.getCurrent();//new Display();
+public class HelpBrowser {
+
+	private Display display = Display.getCurrent();// new Display();
 	private final Shell shell = new Shell(display);
 	private Browser browser;
 	private ArrayList<String> urls = new ArrayList<String>();
 	private String[] titles;
 	private int index;
-	
+
 	Label labelStatus;
 	Text textLocation;
-    
-	
-    
-	public HelpBrowser(){
-		
+
+	public HelpBrowser() {
+
 		buildUrls();
 	}
 
 	@SuppressWarnings("deprecation")
-	private void buildUrls(){
+	private void buildUrls() {
 		urls.add("www.google.com");
 		DirectoryDialog dialog = new DirectoryDialog(shell);
-	      String folder = "C:\\Users\\spencer\\workspace\\jbrick\\help\\html";//dialog.open();
-	      System.out.println(folder);
-	      if (folder == null) return;
-	      File file = new File(folder);
-	      File[] files = file.listFiles(new FilenameFilter() {
-	         public boolean accept(File dir, String name) {
-	            return name.endsWith(".html");
-	         }
-	      });
-	      
-	      titles = new String[files.length];
-	      index = 0;
-	      for (int i = 0; i < files.length; i++) {
-	         try {
-	         String url = files[i].toURL().toString();
-	            urls.add(url);
-	         } catch (MalformedURLException ex) {}
-	      }
-		
+		String folder = "C:\\Documents and Settings\\pxs1160\\workspace\\jbrick\\help\\html";// dialog.open();
+		System.out.println(folder);
+		if (folder == null)
+			return;
+		File file = new File(folder);
+		File[] files = file.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".html");
+			}
+		});
+
+		titles = new String[files.length];
+		index = 0;
+		for (int i = 0; i < files.length; i++) {
+			try {
+				String url = files[i].toURL().toString();
+				urls.add(url);
+			} catch (MalformedURLException ex) {
+			}
+		}
+
 	}
-	
-	public void show(){
-		
+
+	public void show() {
+
 		shell.setText("Jbricx Help Browser");
 		shell.setLayout(new GridLayout());
-		
+
 		ToolBar toolBar = new ToolBar(shell, SWT.FLAT | SWT.RIGHT);
-	    final ToolBarManager manager = new ToolBarManager(toolBar);
-	    
-	    	    
-	    
-	    
-		Composite compTools = new Composite(shell,SWT.NONE);
+		final ToolBarManager manager = new ToolBarManager(toolBar);
+
+		Composite compTools = new Composite(shell, SWT.NONE);
 		GridData data = new GridData(GridData.FILL_HORIZONTAL);
 		compTools.setLayoutData(data);
-		compTools.setLayout(new GridLayout(2,false));
-		
-		
-		Composite compWin = new Composite(shell,SWT.NONE);
+		compTools.setLayout(new GridLayout(2, false));
+
+		Composite compWin = new Composite(shell, SWT.NONE);
 		data = new GridData(GridData.FILL_BOTH);
-		
+
 		compWin.setLayoutData(data);
 		compWin.setLayout(new FillLayout());
 		final SashForm form = new SashForm(compWin, SWT.HORIZONTAL);
 		form.setLayout(new FillLayout());
 
-		
-		final List list = new List(form,SWT.SIMPLE);
-		for (String s: urls){
+		final List list = new List(form, SWT.SIMPLE);
+		for (String s : urls) {
 			list.add(s);
 		}
-		
+
 		browser = new Browser(form, SWT.NONE);
-		
-		list.addListener(SWT.Selection, new Listener(){
+
+		list.addListener(SWT.Selection, new Listener() {
 			@Override
 			public void handleEvent(Event arg0) {
 				int index = list.getSelectionIndex();
 				browser.setUrl(urls.get(index));
-				
 			}
 		});
-		
+
 		Composite compositeStatus = new Composite(shell, SWT.NULL);
-	    compositeStatus.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-	    compositeStatus.setLayout(new GridLayout(2, false));
+		compositeStatus.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		compositeStatus.setLayout(new GridLayout(2, false));
 
 		labelStatus = new Label(compositeStatus, SWT.NULL);
-	    labelStatus.setText("Ready");
-	    labelStatus.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		labelStatus.setText("Ready");
+		labelStatus.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
-	    final ProgressBar progressBar =	new ProgressBar(compositeStatus, SWT.SMOOTH);
-	
+		final ProgressBar progressBar = new ProgressBar(compositeStatus,
+				SWT.SMOOTH);
 
-	   
-	    // Adds tool bar items using actions.
-	    final Action actionBackward =
-	      new Action(
-	        "&Back",
-	        ImageDescriptor.createFromFile(
-	        		HelpBrowser.class,
-	          "/images/go-previous.png")) {
-	      public void run() {
-	        browser.back();
-	      }
-	    };
-	    actionBackward.setEnabled(true); // action is disabled at start up.
-	    
-	    final Action actionForward =
-	        new Action(
-	          "&Forward",
-	          ImageDescriptor.createFromFile(
-		        		HelpBrowser.class,
-		          "/images/go-next.png")) {
-	        public void run() {
-	          browser.forward();
-	        }
-	      };
-	      actionForward.setEnabled(true); // action is disabled at start up.
+		// Adds tool bar items using actions.
+		final Action actionBackward = new Action("&Back", ImageDescriptor
+				.createFromFile(HelpBrowser.class, "/images/go-previous.png")) {
+			public void run() {
+				browser.back();
+			}
+		};
+		actionBackward.setEnabled(true); // action is disabled at start up.
 
-	      Action actionStop =
-	        new Action(
-	          "&Stop",
-	          ImageDescriptor.createFromFile(
-		        		HelpBrowser.class,
-		          "/images/process-stop.png")) {
-	        public void run() {
-	          browser.stop();
-	        }
-	      };
+		final Action actionForward = new Action("&Forward", ImageDescriptor
+				.createFromFile(HelpBrowser.class, "/images/go-next.png")) {
+			public void run() {
+				browser.forward();
+			}
+		};
+		actionForward.setEnabled(true); // action is disabled at start up.
 
-	      Action actionRefresh =
-	        new Action(
-	          "&Refresh",
-	          ImageDescriptor.createFromFile(
-		        		HelpBrowser.class,
-		          "/images/view-refresh.png")) {
-	        public void run() {
-	          browser.refresh();
-	        }
-	      };
+		Action actionStop = new Action("&Stop", ImageDescriptor.createFromFile(
+				HelpBrowser.class, "/images/process-stop.png")) {
+			public void run() {
+				browser.stop();
+			}
+		};
 
-	      Action actionHome =
-	        new Action(
-	          "&Home",
-	          ImageDescriptor.createFromFile(
-		        		HelpBrowser.class,
-		          "/images/go-home.png")) {
-	        public void run() {
-	          browser.setUrl("http://www.google.com/phone");
-	        }
-	      };
-	      
-	      
-	    
-	    manager.add(actionBackward);
-	    manager.add(actionForward);
-	    manager.add(actionStop);
-	    manager.add(actionRefresh);
-	    manager.add(actionHome);
-	    manager.update(true);
-	    toolBar.pack();
-	    
-	    
-	    
-		
-		
-	    
-			
-		
-		
-	    
+		Action actionRefresh = new Action("&Refresh", ImageDescriptor
+				.createFromFile(HelpBrowser.class, "/images/view-refresh.png")) {
+			public void run() {
+				browser.refresh();
+			}
+		};
+
+		Action actionHome = new Action("&Home", ImageDescriptor.createFromFile(
+				HelpBrowser.class, "/images/go-home.png")) {
+			public void run() {
+				browser.setUrl("http://www.google.com/phone");
+			}
+		};
+
+		Action actionPrint = new Action("&Print", ImageDescriptor
+				.createFromFile(HelpBrowser.class, "/images/e_print_topic.png")) {
+			public void run() {
+				// Printer
+
+				PrintDialog printDialog = new PrintDialog(shell, SWT.SELECTED);
+				printDialog.setText("Print");
+				PrinterData printerData = printDialog.open();
+				if (!(printerData == null)) {
+					Printer p = new Printer(printerData);
+					p.startJob("PrintJob");
+					p.startPage();
+
+					Rectangle trim = p.computeTrim(0, 0, 0, 0);
+					Point dpi = p.getDPI();
+					int leftMargin = dpi.x + trim.x;
+					int topMargin = dpi.y / 2 + trim.y;
+					GC gc = new GC(p);
+					Font font = gc.getFont();
+					String printText = "";
+
+					gc.drawString(printText, leftMargin, topMargin
+							+ font.getFontData()[0].getHeight());
+					p.endPage();
+					gc.dispose();
+					p.endJob();
+					p.dispose();
+				}
+			}
+		};
+
+		actionPrint.setEnabled(true);
+
+		manager.add(actionBackward);
+		manager.add(actionForward);
+		manager.add(actionStop);
+		manager.add(actionRefresh);
+		manager.add(actionHome);
+		manager.add(actionPrint);
+		manager.update(true);
+		toolBar.pack();
+
 		browser.addProgressListener(new ProgressListener() {
-		    public void changed(ProgressEvent event) {
-		      progressBar.setMaximum(event.total);
-		      progressBar.setSelection(event.current);
-		    }
+			public void changed(ProgressEvent event) {
+				progressBar.setMaximum(event.total);
+				progressBar.setSelection(event.current);
+			}
 
-		    public void completed(ProgressEvent event) {
-		      progressBar.setSelection(0);
-		    }
-		  });
+			public void completed(ProgressEvent event) {
+				progressBar.setSelection(0);
+			}
+		});
 
-		  browser.addStatusTextListener(new StatusTextListener() {
-		    public void changed(StatusTextEvent event) {
-		      labelStatus.setText(event.text);
-		    }
-		  });
+		browser.addStatusTextListener(new StatusTextListener() {
+			public void changed(StatusTextEvent event) {
+				labelStatus.setText(event.text);
+			}
+		});
 
-		  browser.addTitleListener(new TitleListener() {
-		    public void changed(TitleEvent event) {
-		      shell.setText(event.title + " - powered by SWT");
-		    }
-		  });
-	      
-		  browser.addOpenWindowListener(new OpenWindowListener() {
-			
+		browser.addTitleListener(new TitleListener() {
+			public void changed(TitleEvent event) {
+				shell.setText(event.title + " - powered by SWT");
+			}
+		});
+
+		browser.addOpenWindowListener(new OpenWindowListener() {
+
 			@Override
 			public void open(WindowEvent we) {
 				// TODO Auto-generated method stub
 				we.browser = browser;
 			}
-		  });
-	      
-	      
-		
-		
-		
+		});
+
 		shell.open();
-		while (!shell.isDisposed ()) {
-			if (!display.readAndDispatch ()) display.sleep ();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
 		}
 		display.dispose();
 	}
-	
 
-	
 	public static void main(String[] args) {
 		Display d = new Display();
 		HelpBrowser hb = new HelpBrowser();
 		hb.show();
-		
-		
-		
 	}
 }
