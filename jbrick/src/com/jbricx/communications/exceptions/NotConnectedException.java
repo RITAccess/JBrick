59df2434fc2c43039a2e42c75bad4642a8cb6473
migit128 <devnull@localhost>
2010-07-13 15:35:39 +0000

    Serial driver for the future wii nunchuk code created based off of the RXTXcomm dll

---
+++ jbrick/src/com/jbricx/communications/exceptions/NotConnectedException.java
@@ -0,0 +1,9 @@
+package com.jbricx.communications.exceptions;
+
+public class NotConnectedException extends Exception{
+
+	public NotConnectedException(final String message) {
+		super(message);
+	}
+}
+
