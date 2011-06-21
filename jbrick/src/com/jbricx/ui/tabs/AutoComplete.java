81102e16231d0d924707d91af5e6800876ca9fb6
brainsnbrawns2 <brainsnbrawns2@gmail.com>
2011-06-21 20:45:39 +0000

    Added listener to SourceViewer for catching Ctrl-Space

--- jbrick/src/com/jbricx/ui/tabs/AutoComplete.java
+++ jbrick/src/com/jbricx/ui/tabs/AutoComplete.java
@@ -2,18 +2,40 @@ package com.jbricx.ui.tabs;
 
 import java.util.SortedSet;
 import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 
 public class AutoComplete {
-	private SortedSet keywords;
-	private SourceViewer viewer;
-	private String selectedWord;
-	public static void loadKeywords(String path){
-	
+	private static SortedSet keywords;
+	private static SourceViewer viewer;
+	private static String selectedWord;
+
+	public static void loadKeywords(String path) {
+
 	}
-	public static void attachAutoComplete(SourceViewer viewer){
-		
+
+	public static void attachAutoComplete(SourceViewer viewer) {
+		viewer.getTextWidget().addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if((char)e.keyCode == ' ' && (e.stateMask & SWT.CTRL) != 0){
+					//Ctrl-Space
+					displayAutoComplete();
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+			}
+
+		});
 	}
-	public static void selectClosest(String typedWord){
-		
+	private static void displayAutoComplete(){
+
+	}
+	public static void selectClosest(String typedWord) {
+
 	}
 }
