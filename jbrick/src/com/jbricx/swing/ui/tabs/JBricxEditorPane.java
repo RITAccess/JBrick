a6ad417cc6970375e00f95dd44a8e8156d73d2a5
dgl2311 <dgl2311@g.rit.edu>
2012-03-26 10:56:14 -0400

    Starting tab items.

--- jbrick/src/com/jbricx/swing/ui/tabs/JBricxEditorPane.java
+++ jbrick/src/com/jbricx/swing/ui/tabs/JBricxEditorPane.java
@@ -1,7 +1,113 @@
 package com.jbricx.swing.ui.tabs;
 
+import javax.swing.JEditorPane;
 import javax.swing.JTabbedPane;
+
 
 public class JBricxEditorPane extends JTabbedPane {
 
+	boolean open(final String filename){
+		return false;
+		
+	}
+
+	void closeFile(String filename){
+		
+	}
+
+	boolean openNewFile(){
+		return false;
+		
+	}
+
+	boolean checkOverwrite(){
+		return false;
+		
+	}
+	
+	JBricxTabItem getSelection(){
+		return null;
+		
+	}
+/*
+	SourceViewer getSourceViewer(){
+		
+	}
+
+	void addCTabFolder2Listener(CTabFolder2Adapter cTabFolder2Adapter){
+		
+	}
+*/
+	void setMaximized(boolean b){
+		
+	}
+
+	void setMinimized(boolean b){
+		
+	}
+
+	void setSelection(int selectedIndex){
+		
+	}
+
+	JBricxTabItem[] getItems(){
+		return null;
+		
+	}
+
+	int getSelectionIndex(){
+		return tabPlacement;
+		
+	}
+
+	void saveFile(String filePath){
+		
+	}
+
+	void insertText(String text){
+		
+	}
+
+	int contains(String fileName){
+		return tabPlacement;
+		
+	}
+
+	void undo(){
+		
+	}
+
+	void redo(){
+		
+	}
+
+	void cut(){
+		
+	}
+
+	void copy(){
+		
+	}
+
+	void paste(){
+		
+	}
+
+	void selectAll(){
+		
+	}
+
+	String getCurrentFilename(){
+		return null;
+		
+	}
+
+	int getCurrentIndex(){
+		return tabPlacement;
+		
+	}
+
+	void refreshTabItems(){
+		
+	}
 }
---
+++ jbrick/src/com/jbricx/swing/ui/tabs/JBricxTabItem.java
@@ -0,0 +1,12 @@
+package com.jbricx.swing.ui.tabs;
+
+import javax.swing.JEditorPane;
+
+/**
+ * An individual tab item to be used in the editor pane.
+ * @author Daniel
+ *
+ */
+public class JBricxTabItem extends JEditorPane {
+
+}
