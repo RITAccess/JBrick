cb0dc00343a99b13349dfd82a4a098693fa4218a
Jim <devnull@localhost>
2012-04-24 13:01:26 -0400

    got the tree to display the files in the directory but clicking on them does nothing

--- jbrick/src/com/jbricx/swing/ui/tabs/JBricxFilePane.java
+++ jbrick/src/com/jbricx/swing/ui/tabs/JBricxFilePane.java
@@ -1,7 +1,20 @@
 package com.jbricx.swing.ui.tabs;
 
+import java.io.File;
+import java.util.prefs.Preferences;
+
 import javax.swing.JTree;
+import com.jbricx.swing.ui.preferences.PreferenceStore;
 
 public class JBricxFilePane extends JTree {
+	
+	Preferences prefs;
+	File root;
+	
+	public JBricxFilePane(){
+		root = new File(PreferenceStore.getPrefs().get(PreferenceStore.WRKSPC, PreferenceStore.WRKSPC_DEFAULT));
+		FileTreeModel model = new FileTreeModel(root);
+		this.setModel(model);
+	}
 
 }
