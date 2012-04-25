25220266c6d22852e86b4e989ec3172e9af13fb3
Jim <devnull@localhost>
2012-04-25 15:46:41 -0400

    got the scroll bars to work for the file tree

--- jbrick/src/com/jbricx/swing/ui/tabs/JBricxFilePane.java
+++ jbrick/src/com/jbricx/swing/ui/tabs/JBricxFilePane.java
@@ -7,35 +7,32 @@ import java.util.prefs.Preferences;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
-import javax.swing.tree.TreePath;
-
 import com.jbricx.swing.ui.JBricxManager;
 import com.jbricx.swing.ui.preferences.PreferenceStore;
 
-public class JBricxFilePane extends JTree implements MouseListener{
+public class JBricxFilePane extends JScrollPane implements MouseListener{
 	
 	Preferences prefs;
 	File root;
 	JBricxManager manager;
+	JTree tree;
 	
 	public JBricxFilePane(JBricxManager manager){
+		super( JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		tree = new JTree();
 		this.manager = manager;
 		root = new File(PreferenceStore.getPrefs().get(PreferenceStore.WRKSPC, PreferenceStore.WRKSPC_DEFAULT));
 		FileTreeModel model = new FileTreeModel(root);
-		this.setModel(model);
-		
-		this.add(new JScrollPane(
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
-		
-		this.addMouseListener(this);
+		tree.setModel(model);
+		tree.addMouseListener(this);
+		this.setViewportView(tree);
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		if( arg0.getClickCount() >= 2){
-			File ftoOpen = (File) this.getLastSelectedPathComponent();
+			File ftoOpen = (File) tree.getLastSelectedPathComponent();
 			manager.openTab(ftoOpen.getAbsolutePath());
 			
 		}
