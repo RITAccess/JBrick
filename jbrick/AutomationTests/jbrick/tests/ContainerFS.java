90cf2a4f88f8eee201c9be744f926c6881e7bb26
Jesse Jurman <j.r.jurman@gmail.com>
2014-03-07 08:26:14 -0500

    finished ContainerFS template

--- jbrick/AutomationTests/jbrick/tests/ContainerFS.java
+++ jbrick/AutomationTests/jbrick/tests/ContainerFS.java
@@ -17,6 +17,9 @@ public class ContainerFS {
 		return false;
 	}
 	
+	/**
+	 * TC102 - Reset Main Window View
+	 */
 	public static boolean TC102() {
 		
 		// 1. User opens the JBrick application 
@@ -38,6 +41,27 @@ public class ContainerFS {
 		
 		//TODO select the "reset-view" option in the view menu
 		//TODO check the size and position of frames
+		
+		return false;
+	}
+	
+	/**
+	 * TC103 - Open the Expander View
+	 */
+	public static boolean TC103() {
+		
+		// 1. User opens the JBrick application 
+		// => The Code Frame has one file open in the code frame (center) and a status pane (bottom)
+		// (No file has been opened before hand, this is the equivalent to opening the application for the first time.)
+		
+		//TODO open the application
+		//TODO check the frame positions
+		
+		// 2. User selects the "Show/Hide File Viewer"
+		// => The file viewer is opened on the left of the code and status frame. The viewer shows the files and folders in the workspace directory set in the preferences.
+		
+		//TODO select the file viewer menu option
+		//TODO check that the frame is opened and has a width value
 		
 		return false;
 	}
