b60b55b66bff84e1201762dea308aed0975822da
Lindsey <lle6138@rit.edu>
2014-03-07 08:31:57 -0500

    Adding SettingsFS

---
+++ jbrick/AutomationTests/jbrick/tests/SettingsFS.java
@@ -0,0 +1,34 @@
+package jbrick.tests;
+
+public class SettingsFS {
+
+	/**
+	 * TC201 - Reset Default Font
+	 */
+	public static boolean TC201() {
+		
+		// 1. User opens the JBrick application 
+		// => The Code Frame has one file open in the code frame (center), a status pane (bottom), and a picture menu frame (top)
+		// (No file has been opened before hand, this is the equivalent to opening the application for the first time.)
+		
+		//TODO open the application
+		
+		// 2. User opens preferences and changes the font options
+		// => The Code Frame has a font that differs from the default font (Courier-plain-12)
+		
+		//TODO open the preferences window
+		//TODO change the font
+		//TODO select "apply" button and then "ok" button
+		//TODO check that the font changed
+		
+		// 3. User opens preferences and sets font to default settings
+		// => The Code Frame has a font that is the same as the default font 
+		
+		//TODO open the preferences window
+		//TODO select the "reset to default" button
+		//TODO select "apply" button and then "ok" button
+		//TODO check that the font is the default font
+		
+		return false;
+	}
+}
