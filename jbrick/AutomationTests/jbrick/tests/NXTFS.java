b20196fdb61ff141f4336c419cca036f09fb35ad
Jesse Jurman <j.r.jurman@gmail.com>
2014-03-13 17:54:35 -0400

    started NXT Feature Suite

---
+++ jbrick/AutomationTests/jbrick/tests/NXTFS.java
@@ -0,0 +1,21 @@
+package jbrick.tests;
+
+public class NXTFS {
+
+	/**
+	 * TC601 - Compile
+	 */
+	public static boolean TC601() {
+		
+		return false;
+	}
+	
+	/**
+	 * TC602 - Download
+	 */
+	public static boolean TC602() {
+		
+		return false;
+	}
+	
+}
