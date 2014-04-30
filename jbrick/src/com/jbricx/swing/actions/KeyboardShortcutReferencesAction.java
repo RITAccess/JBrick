c45c55cca9a552a28694c9c6c4c82d15da457a6a
Lindsey <lle6138@rit.edu>
2014-04-30 10:24:02 -0400

    Reference Screen added!
     Add a reference screen #21

--- jbrick/src/com/jbricx/swing/actions/KeyboardShortcutReferencesAction.java
+++ jbrick/src/com/jbricx/swing/actions/KeyboardShortcutReferencesAction.java
@@ -2,7 +2,10 @@ package com.jbricx.swing.actions;
 import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.swing.ImageIcon;
 
@@ -11,7 +14,7 @@ import com.jbricx.swing.ui.JBricxManager;
 @SuppressWarnings("serial")
 public class KeyboardShortcutReferencesAction extends JBricxAbstractAction 
 {
-	String fileName = "/KeyboardShortcutsHelp.html";
+	String filePath = "KeyboardShortcutsHelp.html";
 
 	/**
 	 * KeyboardShortcutReferencesAction constructor
@@ -21,35 +24,47 @@ public class KeyboardShortcutReferencesAction extends JBricxAbstractAction
 		//Name set to "" so that "About" does not show in toolbar icon.
 		super("",new ImageIcon(AboutAction.class.getResource("/images/help-browser.png")), manager);
 	}
+	
+    private void copyInputstreamToFile (InputStream in, OutputStream out) {
+        try {
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        } catch (IOException ex) {
+           
+        }
+    }
 
 	/**
 	 * Open the html reference page for keyboard shortcuts
 	 */
 	public void actionPerformed(ActionEvent arg0) 
-	{
-		File f = new File(fileName);
-       
-		//first check if Desktop is supported by Platform or not
-        if(!Desktop.isDesktopSupported())
-        {
-            System.out.println("Desktop is not supported");
-            return;
-        }
-         
-        Desktop desktop = Desktop.getDesktop();
-        
-        if(f.exists())
-        {
-            System.out.println("File found");
-        	
-			try 
-			{
-				desktop.open(f);
-			} catch (IOException e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	{		
+		InputStream is = getClass().getResourceAsStream(filePath);  
+		File f;
+		try {
+			f = File.createTempFile("yourappname", ".html");
+			f.deleteOnExit(); 
+			FileOutputStream os = new FileOutputStream(f);
+			copyInputstreamToFile(is,os);
+			
+			//first check if Desktop is supported by Platform or not
+	        if(!Desktop.isDesktopSupported())
+	        {
+	            System.out.println("Desktop is not supported");
+	            return;
+	        }
+			
+	        Desktop desktop = Desktop.getDesktop();
+			desktop.open(f);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}  
+
 	}
 }
