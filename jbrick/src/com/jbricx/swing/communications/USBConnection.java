6ae292d106acde187462761a2ae30b39f0ef32c0
Ethan Jurman <ehj2229@g.rit.edu>
2014-06-18 13:20:44 -0400

    working usb connection on mac

--- jbrick/src/com/jbricx/swing/communications/USBConnection.java
+++ jbrick/src/com/jbricx/swing/communications/USBConnection.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
 import org.usb4java.Context;
+import org.usb4java.ControlSetup;
 import org.usb4java.Device;
 import org.usb4java.DeviceDescriptor;
 import org.usb4java.DeviceHandle;
@@ -56,31 +57,57 @@ public class USBConnection
      */
     public static void main(String[] args)
     {
+    	Context context = new Context();
+    	int result = LibUsb.init(context);
+    	if (result != LibUsb.SUCCESS){
+    		throw new LibUsbException("unable to initialize libusb", result);
+    	}
+    	
+    	
+    	
         Device device = findDevice((short) 0x0694, (short) 0x0002);
         System.out.println(device);
         DeviceHandle dh = new DeviceHandle();
         LibUsb.open(device, dh);
-        ByteBuffer buffer = ByteBuffer.allocateDirect(8);
+        int usbInterface = 0;
+        LibUsb.claimInterface(dh, usbInterface); // handle, Interface
+        if (result != LibUsb.SUCCESS)
+        {
+            throw new LibUsbException("Unable to claim interface", result);
+        }
+        
+        // Check if kernel driver must be detached
+        boolean detach = LibUsb.hasCapability(LibUsb.CAP_SUPPORTS_DETACH_KERNEL_DRIVER);
+
+        // Detach the kernel driver
+        if (detach)
+        {
+            result = LibUsb.detachKernelDriver(dh,  usbInterface);
+            if (result != LibUsb.SUCCESS) throw new LibUsbException("Unable to detach kernel driver", result);
+        }
+        
+        ByteBuffer buffer = ByteBuffer.allocateDirect(6);
         buffer.put(new byte[]{(byte) 0x80, (byte) 0x03, (byte) 0xf4, (byte) 0x01, (byte) 0xf4, (byte) 0x01});
+        /*
         int value = LibUsb.controlTransfer(
         		dh, // handle - A handle for the device to communicate with.
         		(byte) (LibUsb.RECIPIENT_DEVICE), // bmRequestType - The request type field for the setup packet.
-        		(byte) 0x09, //bRequest - The request field for the setup packet.
+        		(byte) LibUsb.REQUEST_TYPE_STANDARD, //bRequest - The request field for the setup packet.
         		(short) 2, // wValue - The value field for the setup packet.
         		(short) 1, // wIndex - The index field for the setup packet.
         		buffer, // data - A suitably-sized data buffer for either input or output (depending on direction bits within bmRequestType).
         		5000); // timeout - Timeout (in millseconds) that this function should wait before giving up due to no response being received. For an unlimited timeout, use value 0.
-        
-        
-        //int value2 = LibUsb.bulkTransfer(dh, LibUsb.ENDPOINT_ADDRESS_MASK, buffer, null, 5000);
-        
-        int[] errors = new int[]{LibUsb.ERROR_TIMEOUT, LibUsb.ERROR_PIPE, LibUsb.ERROR_NO_DEVICE};
-        for (int e : errors){
-        	if (e == value){
-        		System.out.println("ERROR");
-        	}
-        }
-        System.out.println(value);
+        */
+        int value = LibUsb.bulkTransfer(
+        		dh, // handle - A handle for the device to communicate with.
+        		(byte) 0x01, // endpoint - The address of a valid endpoint to communicate with.
+        		buffer, // data - A suitably-sized data buffer for either input or output (depending on endpoint).
+        		IntBuffer.allocate(1), // transferred - Output location for the number of bytes actually transferred.
+        		5000 // timeout - timeout (in millseconds) that this function should wait before giving up due to no response being received. For an unlimited timeout, use value 0.
+        );
+        System.out.print(LibUsb.errorName(value));
+        System.out.println(", " + value);
         LibUsb.close(dh);
+        LibUsb.exit(context);
     }
 }
