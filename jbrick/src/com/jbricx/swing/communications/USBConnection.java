c69c8571c386b467090517dbf7423ebafcb767ab
Ethan Jurman <ehj2229@g.rit.edu>
2014-06-18 13:35:06 -0400

    made it look a little better

--- jbrick/src/com/jbricx/swing/communications/USBConnection.java
+++ jbrick/src/com/jbricx/swing/communications/USBConnection.java
@@ -19,6 +19,30 @@ import org.usb4java.LibUsbException;
  */
 public class USBConnection
 {
+	/*
+	 * NXT -- VENDOR : 0x0694, PRODUCT : 0x0002, INTERFACE : 0, ENDPOINT_OUT : 0x01, ENDPOINT_IN : 0x82
+	 * EV3 -- VENDOR : 0x1684, PRODUCT : 0x0005 
+	 */
+	enum MindStormDevice{
+		NXT((short) 0x0694, (short) 0x0002, 0, (byte) 0x01, (byte) 0x82),
+		//EV3((short) 1684, (short) 0x0005, null, null, null), // TODO unsure about the interface and endpoints
+		;
+		
+		public short vendor;
+		public short product;
+		public int usbInterface;
+		public byte endOut;
+		public byte endIn;
+
+		MindStormDevice(short vendor, short product, int usbInterface, byte endOut, byte endIn){
+			this.vendor = vendor;
+			this.product = product;
+			this.usbInterface = usbInterface;
+			this.endOut = endOut;
+			this.endIn = endIn;
+		}
+	}
+	
 	public static Device findDevice(short vendorId, short productId)
 	{
 	    // Read the USB device list
@@ -63,13 +87,11 @@ public class USBConnection
     		throw new LibUsbException("unable to initialize libusb", result);
     	}
     	
-    	
-    	
-        Device device = findDevice((short) 0x0694, (short) 0x0002);
+        Device device = findDevice(USBConnection.MindStormDevice.NXT.vendor, USBConnection.MindStormDevice.NXT.product);
         System.out.println(device);
         DeviceHandle dh = new DeviceHandle();
         LibUsb.open(device, dh);
-        int usbInterface = 0;
+        int usbInterface = USBConnection.MindStormDevice.NXT.usbInterface;
         LibUsb.claimInterface(dh, usbInterface); // handle, Interface
         if (result != LibUsb.SUCCESS)
         {
@@ -88,16 +110,6 @@ public class USBConnection
         
         ByteBuffer buffer = ByteBuffer.allocateDirect(6);
         buffer.put(new byte[]{(byte) 0x80, (byte) 0x03, (byte) 0xf4, (byte) 0x01, (byte) 0xf4, (byte) 0x01});
-        /*
-        int value = LibUsb.controlTransfer(
-        		dh, // handle - A handle for the device to communicate with.
-        		(byte) (LibUsb.RECIPIENT_DEVICE), // bmRequestType - The request type field for the setup packet.
-        		(byte) LibUsb.REQUEST_TYPE_STANDARD, //bRequest - The request field for the setup packet.
-        		(short) 2, // wValue - The value field for the setup packet.
-        		(short) 1, // wIndex - The index field for the setup packet.
-        		buffer, // data - A suitably-sized data buffer for either input or output (depending on direction bits within bmRequestType).
-        		5000); // timeout - Timeout (in millseconds) that this function should wait before giving up due to no response being received. For an unlimited timeout, use value 0.
-        */
         int value = LibUsb.bulkTransfer(
         		dh, // handle - A handle for the device to communicate with.
         		(byte) 0x01, // endpoint - The address of a valid endpoint to communicate with.
