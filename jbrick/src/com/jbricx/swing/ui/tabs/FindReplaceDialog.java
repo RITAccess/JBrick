e0ea9a070606b9c401e242d77001f9f589b6d6d9
melissay94 <melissay94@yahoo.com>
2014-07-22 14:14:28 -0400

    Now loops to beginning and selects the word initially #44

--- jbrick/src/com/jbricx/swing/ui/tabs/FindReplaceDialog.java
+++ jbrick/src/com/jbricx/swing/ui/tabs/FindReplaceDialog.java
@@ -28,8 +28,8 @@ import com.jbricx.tools.AccessibleWidget;
 
 
 /**
- * Modified for JBricx
  * @author Pradeep Yadav
+ * @author Melissa Young
  */
 @SuppressWarnings("serial")
 public class FindReplaceDialog extends JDialog {
@@ -54,8 +54,12 @@ public class FindReplaceDialog extends JDialog {
 		this.textArea = textArea;
 		setUpFindReplace();
 	}
-
+/**
+ * Instantiate and organize all components for the pop up dialog window
+ * Call the action listeners for the buttons 
+ */
 	private void setUpFindReplace() {
+		
 		findButton = new JButton("Find Next");
 		replaceButton = new JButton("Replace");
 		replaceAllButton = new JButton("Replace All");
@@ -64,58 +68,7 @@ public class FindReplaceDialog extends JDialog {
 		findLabel = new JLabel("Find : ");
 		replaceLabel = new JLabel("Replace : ");
 		accessibleStatus = new AccessibleWidget(" ");
-
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		setTitle("Find/Replace");
-		setModal(true);
-
-		GroupLayout layout = new GroupLayout(getContentPane());
-		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-						.addGap(14, 14, 14)
-						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-								.addComponent(replaceLabel)
-								.addComponent(findLabel, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE))
-								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-										.addComponent(findTextField)
-										.addComponent(accessibleStatus)
-										.addComponent(replaceTextField, GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE))
-										.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-												.addGroup(layout.createSequentialGroup()
-														.addGap(8, 8, 8)
-														.addComponent(findButton, GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE))
-														.addGroup(layout.createSequentialGroup()
-																.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-																.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-																		.addComponent(replaceAllButton, GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
-																		.addComponent(replaceButton, GroupLayout.PREFERRED_SIZE, 98, Short.MAX_VALUE))))
-																		.addContainerGap())
-		);
-		layout.setVerticalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup()
-						.addContainerGap()
-						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-								.addComponent(findTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(findButton)
-								.addComponent(findLabel))
-								
-								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-										.addComponent(replaceLabel)
-										.addComponent(replaceTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(replaceButton))
-										
-										.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-										.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-												.addComponent(accessibleStatus)
-												.addComponent(replaceAllButton))
-												.addContainerGap())
-		);
-
+		
 		findButton.addActionListener(new ActionListener() { 
 			public void actionPerformed(ActionEvent evt) {
 				findButtonAction(evt);
@@ -139,11 +92,68 @@ public class FindReplaceDialog extends JDialog {
 			public void keyReleased(KeyEvent evt) {
 				findTextKeyAction(evt);
 			}
-		});
+		}); 
+		
+		// Basic set up for the dialog box
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setTitle("Find/Replace");
+		setModal(true);
+
+		// Layout for the components in the dialog box
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+			layout.setHorizontalGroup(
+				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+						.addGap(14, 14, 14)
+						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+								.addComponent(replaceLabel)
+								.addComponent(findLabel, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE))
+								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+										.addComponent(findTextField)
+										.addComponent(accessibleStatus)
+										.addComponent(replaceTextField, GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE))
+										.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+												.addGroup(layout.createSequentialGroup()
+														.addGap(8, 8, 8)
+														.addComponent(findButton, GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE))
+														.addGroup(layout.createSequentialGroup()
+																.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+																.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+																		.addComponent(replaceAllButton, GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
+																		.addComponent(replaceButton, GroupLayout.PREFERRED_SIZE, 98, Short.MAX_VALUE))))
+																		.addContainerGap())
+		);
+			layout.setVerticalGroup(
+				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+						.addContainerGap()
+						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+								.addComponent(findTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(findButton)
+								.addComponent(findLabel))
+								
+								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+										.addComponent(replaceLabel)
+										.addComponent(replaceTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(replaceButton))
+										
+										.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+										.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+												.addComponent(accessibleStatus)
+												.addComponent(replaceAllButton))
+												.addContainerGap())
+		);
 
 		pack();
 	}
 
+	/**
+	 * Set the current position for the set up
+	 * @param evt
+	 */
 	private void findTextKeyAction(KeyEvent evt) {
 		currentPos = 0;
 	}
@@ -166,6 +176,7 @@ public class FindReplaceDialog extends JDialog {
 			accessibleStatus.readLabel("Cannot Find to Replace",FindReplaceDialog.this.replaceAllButton);
 		}
 		
+		// If the word exist, it replaces all instances of the word with the user input
 		else {
 			accessibleStatus.readLabel(" ", FindReplaceDialog.this.replaceAllButton);
 			String newContent = oldContent.replaceAll( findTextField.getText() , replaceTextField.getText() );
@@ -178,6 +189,8 @@ public class FindReplaceDialog extends JDialog {
 				replaced = textArea.getText().indexOf(replaceTextField.getText(),replaced + 1);
 				count ++;
 			}
+			
+			// Send a message letting the user know how many instances there were to replace
 			if (count > 0) {
 				String instanceOf = count + " instances of " + findTextField.getText() + " have been replaced with " +  replaceTextField.getText();
 				accessibleStatus.readLabel(instanceOf, FindReplaceDialog.this.replaceAllButton);
@@ -190,20 +203,24 @@ public class FindReplaceDialog extends JDialog {
 	 * @param evt
 	 */
 	private void replaceButtonAction(ActionEvent evt) {
-		if ( !findTextField.getText().isEmpty() &&
-				!replaceTextField.getText().isEmpty() &&
-					!(textArea.getSelectedText()== null))
-		{
-
-			String replaceWord = replaceTextField.getText();
-
-			if ( pos != -1 )
-			{
-				
-				textArea.replaceSelection( replaceWord );
-			}
+		
+		// Check for an empty find field
+		if (findTextField.getText().isEmpty()) {
+			accessibleStatus.readLabel("Find Field is Empty",FindReplaceDialog.this.replaceButton);
 		} else {
-			accessibleStatus.readLabel("Cannot Find to Replace", FindReplaceDialog.this.replaceButton);
+			
+			// Check for it the text exists in the main area
+			if (textArea.getText().contains(findTextField.getText())) {
+				String replaceWord = replaceTextField.getText();
+
+				// If word is found, then it will be replaced and jump to the next instance of the word
+				if (pos != -1) {
+					textArea.replaceSelection(replaceWord);
+					findButtonAction(evt);
+				}
+			} else {
+				accessibleStatus.readLabel("Cannot Find to Replace",FindReplaceDialog.this.replaceButton);
+			}
 		}
 	}
 
@@ -214,28 +231,36 @@ public class FindReplaceDialog extends JDialog {
 	private void findButtonAction(ActionEvent evt) {
 		String wordToFind = findTextField.getText();
 		String context = textArea.getText();
-
+		
 		// Check if anything to search was entered
 		if (wordToFind.isEmpty()) {
-			accessibleStatus.readLabel("Field is missing", FindReplaceDialog.this.findButton);
+			accessibleStatus.readLabel("Find Field is missing", FindReplaceDialog.this.findButton);
 		} else {
-			// Find the word entered into the field and highlight it
-			if (currentPos < context.length()) {
-				accessibleStatus.readLabel(" ", FindReplaceDialog.this.findButton);
-				textArea.requestFocusInWindow();
-				pos = context.indexOf( wordToFind, currentPos);
 			
-				// Check for if the position of the word is valid
-				if (pos != -1) {
-					textArea.select(pos, pos + wordToFind.length());
-					textArea.getSelectedText();
-					currentPos = pos + 1;
+			// Check if any instances of the text entered occurs in the main area
+			if(context.contains(wordToFind)) {
+				
+					// Find the word entered into the field and highlight it
+				if (currentPos < context.length()) {
+					accessibleStatus.readLabel(" ", FindReplaceDialog.this.findButton);
+					textArea.requestFocus();
+					pos = context.indexOf( wordToFind, currentPos);
+					
+					// Check for if the position of the word is valid
+						if (pos != -1) {
+							textArea.select(pos, pos + wordToFind.length());
+							currentPos = pos + 1;
+						}
+						// If the position is the last found text, it will return to the top found text
+						else {
+							currentPos = pos;
+							pos = context.indexOf(wordToFind,currentPos);
+							textArea.select(pos,pos + wordToFind.length());
+							currentPos = pos + 1;
+						}
+					}
 				} else {
 					accessibleStatus.readLabel("Cannot Find",FindReplaceDialog.this.findButton);
-				}
-			} 
-			else if (currentPos >= context.length()) {
-				currentPos = 0;
 			}
 		}
 	}
